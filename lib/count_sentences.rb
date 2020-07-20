a3bee367682be4c553d62bb41de952c93d5b3d3a
Jamie Vullo <jvullo@protonmail.com>
2020-07-20 16:25:32 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,22 +1,18 @@
 require 'pry'
 
-class String
-
-  def sentence?
-     self.end_with?(".")
+class String 
+  
+  def sentence
+    
+  end
+  
+  def question
+    
+  end
+  
+  def exclamation?
     
   end
 
-  def question?
-    self.end_with?("?")
-  end
 
-  def exclamation?
-    self.end_with?("!")
-  end
-
-  def count_sentences
-    self.split(/[?!.] /).count
-    #binding.pry 
-  end
 end
